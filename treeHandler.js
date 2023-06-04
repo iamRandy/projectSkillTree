2dba316717db0b36acee88718e7e7aad9c0cdd2d
Randy Thai <randythai360@gmail.com>
2023-06-04 01:07:49 -0500

    WIP Collision handler

--- treeHandler.js
+++ treeHandler.js
@@ -1,14 +1,41 @@
-// const addBut = document.getElementById("addButton");
-// const friends = document.getElementById("friends");
-// const newElements = document.getElementsByClassName("newElements");
 var container = document.getElementById('newElements');
 var addButton = document.getElementById('addButton');
 var numCircles = 0; 
+var curCircles = []; // List of all the current trees on the screen
 
 addButton.addEventListener('click', function() { // adding functionality to the add button so it creates a "node" 
+    var circleCreated = false; // Flag will be set to true when a new circle has been created that does not collide with any other circle in the scene
+    var newCircle = null; // Temp variable for return statement
 
+    while(!circleCreated){ // While a new circle has not been created yet...
+        if(curCircles.length>0){ // Check if there are any circles in the space
+            newCircle = createCircle(); // Create a new circle
+            for (let cir of curCircles) { // Loop through all the current circles in the space
+                if (checkCollision(cir, newCircle)) { // Check for collision with the new circle and all circles in the space (TODO)
+                    console.log("collision detected!");
+                    circleCreated = true;
+                    break; // Exit the loop early if a collision is detected
+                } else {
+                    circleCreated = true;
+                }
+            }
+            
+        } else { // If this is the first circle being created
+            newCircle = createCircle();
+            circleCreated = true;
+        }
+    }
+
+    if (newCircle) {
+        container.append(newCircle);
+    } else {
+        console.warn("New Circle Could Not Be Created.");
+    }
+    
+});
+
+function createCircle() {
     var multiplier = parseInt(prompt("Size of Circle:"));
-    console.log('Button clicked!');
 
     const newCircle = document.createElement("div");
     newCircle.className = "circle";
@@ -22,6 +49,7 @@ addButton.addEventListener('click', function() { // adding functionality to the 
     newCircle.style.height = size + "px";
 
     var randomValues = getRandom();
+    
     var minX = randomValues[0];
     var maxX = randomValues[1];
     var minY = randomValues[2];
@@ -31,9 +59,9 @@ addButton.addEventListener('click', function() { // adding functionality to the 
     newCircle.style.top = getRandomValue(minY, maxY) + 'px';
     newCircle.style.left = getRandomValue(minX, maxX) + 'px';
 
-    container.append(newCircle);
-    
-});
+    curCircles.push(newCircle); // Pushes the new circle to the array of current circles to keep track
+    return newCircle;
+}
 
 function checkCollision(circle1, circle2) { 
     var x1 = circle1.offsetLeft + circle1.offsetWidth / 2; // getting center of circle
